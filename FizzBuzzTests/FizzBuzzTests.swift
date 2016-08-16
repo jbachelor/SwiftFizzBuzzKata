7a8e9785f9c9f2d1860012d1d92cd106c93ec29a
Jonathan Bachelor <jon_bachelor@me.com>
2016-08-16 08:51:57 -0700

    Initial commit with boilerplate code removed, and a 'prettyError' func defined.

--- FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -11,26 +11,13 @@ import XCTest
 
 class FizzBuzzTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    // MARK: Tests
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    // MARK: Helpers
+
+    func prettyError(test: String, expected: String, actual: String) -> String {
+        return "\n\n**** FAILED \(test)  Expected:  \(expected), actual:  \(actual)\n"
     }
-    
 }
