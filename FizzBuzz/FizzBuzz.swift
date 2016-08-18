3c45627af44817ab07f55f1f74bfa0c1b3f41dab
Jonathan Bachelor <jon_bachelor@me.com>
2016-08-18 06:57:54 -0700

    Refactor - Thanks to our sweet suite of unit tests, we can change yet another implementation in our FizzBuzz class with confidence that the program still behaves according to the rules laid out in our tests. Notice that at this point we have completely changed the implementation of the FizzBuzz class. Aaaaaaaaw yeah!

--- FizzBuzz/FizzBuzz.swift
+++ FizzBuzz/FizzBuzz.swift
@@ -10,28 +10,18 @@ import Foundation
 
 class FizzBuzz {
     
-    func getValue(number: Int) -> String {        
-        if number % 15 == 0 {
-            return "FizzBuzz"
-        } else if number % 3 == 0 {
-            return "Fizz"
-        } else if number % 5 == 0 {
-            return "Buzz"
-        } else {
-            return String(number)
-        }
+
+    func getValue(number: Int) -> String {
+        let result =
+            number % 15 == 0 ? "FizzBuzz" :
+            number % 5 == 0 ? "Buzz" :
+            number % 3 == 0 ? "Fizz" :
+            String(number)
+        
+        
+        return result
     }
     
-
-//    func processFrom(startNumber: Int, to endNumber: Int) -> String {
-//        var fizzBuzzSequence: String = ""
-//        
-//        for number in startNumber...endNumber {
-//            fizzBuzzSequence += "\(getValue(number)), "
-//        }
-//        
-//        return fizzBuzzSequence.substringToIndex(fizzBuzzSequence.endIndex.advancedBy(-2))
-//    }
     
     func processFrom(startNumber: Int, to endNumber: Int) -> String {
         var values: [Int] = [Int]()
